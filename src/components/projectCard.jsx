37ae88ab23536fee5f2ac2689219d7ae711e3a78
tidzi17 <tidzi15110@gmail.com>
2024-02-01 02:09:46 +0100

    added form submition

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -12,12 +12,12 @@ export default function ProjectCard({ id, title, date, image, imageID, descripti
         <div className="w-full h-[230px] border-[1px] border-black rounded-3xl p-4 flex flex-row gap-3 justify-between">
                {isScrollableImage ? (
                <div className="w-1/2 h-full bg-black rounded-3xl overflow-hidden cursor-pointer">
-                   <Link to={liveurl}>   <img src={image} alt="" className="grayscale w-full h-auto transform translate-x-0 transition duration-[10s] ease-in-out hover:-translate-y-[1300px] " /></Link>
+                   <Link to={liveurl} target="blank"><img src={image} alt="" className="grayscale w-full h-auto transform translate-x-0 transition duration-[10s] ease-in-out hover:-translate-y-[1300px] " /></Link>
                 </div>
                 
             ) : (
                  <div className="w-1/2 h-full bg-black rounded-3xl relative">
-                  <Link to={liveurl}>   <img src={image} alt="" className="absolute inset-0 w-full h-full object-cover  rounded-3xl grayscale" /></Link>
+                  <Link to={liveurl} target="blank"><img src={image} alt="" className="absolute inset-0 w-full h-full object-cover  rounded-3xl grayscale" /></Link>
                 </div>
             )}
             <div className="w-1/2 flex flex-col items-center text-center">
