14ae3ff251785e63883148b6905128417fa4a1ef
tidzi17 <tidzi15110@gmail.com>
2024-02-01 14:32:51 +0100

    red

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -12,19 +12,19 @@ export default function Contact(){
       </div>
       <div className="w-full px-6 lg:px-28 py-28 ">
       <div>
-            <div className="w-[120px] h-[2px] bg-black mb-4"></div>
-            <h3 className="text-black text-4xl">get in touch</h3>
+            <div className="w-[80px] md:w-[120px] h-[2px] bg-black mb-4"></div>
+            <h3 className="text-black text-2xl md:text-4xl">get in touch</h3>
         </div>
         <div className="flex flex-col lg:flex-row px-0 pt-10 xl:px-20 lg:pt-28 pb-28 justify-between  ">
-          <div className="w-full lg:w-1/2 mb-5">
-            <h3 className="text-5xl">Let's build something great together!</h3>
-            <p className="text-xl mt-3">Feel free to contact me. I am always open to discussing new projects, creative ideas, or collaboration opportunities that align with your vision.</p>
+          <div className="w-full lg:w-1/2 mb-3 md:mb-5">
+            <h3 className="text-3xl md:text-5xl">Let's build something great together!</h3>
+            <p className="text-base md:text-xl mt-3">Feel free to contact me. I am always open to discussing new projects, creative ideas, or collaboration opportunities that align with your vision.</p>
             <div className="mt-8">
-            <div className="w-[120px] h-[2px] bg-black mb-4"></div>
-            <div className='flex gap-4 '>
-              <a href='https://github.com/tidzi17' target='blank'><img src={giticon} alt="" className='cursor-pointer w-[32px] hover:scale-105'/></a>  
-              <a href='https://www.linkedin.com/in/tijana-djudjic/' target='blank'><img src={linkedicon} alt="" className='cursor-pointer w-[32px] hover:scale-105'/></a>  
-              <a href='mailto:tidzi15110@gmail.com' target='blank'><img src={mailicon} alt="" className='cursor-pointer w-[32px] hover:scale-105'/></a>  
+            <div className="w-[80px] md:w-[120px] h-[2px] bg-black mb-2 md:mb-4"></div>
+            <div className='flex gap-4 mb-6'>
+              <a href='https://github.com/tidzi17' target='blank'><img src={giticon} alt="" className='cursor-pointer w-[28px] md:w-[32px] hover:scale-105'/></a>  
+              <a href='https://www.linkedin.com/in/tijana-djudjic/' target='blank'><img src={linkedicon} alt="" className='cursor-pointer w-[28px] md:w-[32px] hover:scale-105'/></a>  
+              <a href='mailto:tidzi15110@gmail.com' target='blank'><img src={mailicon} alt="" className='cursor-pointer w-[28px] md:w-[32px] hover:scale-105'/></a>  
             </div>
             </div>
           </div>
