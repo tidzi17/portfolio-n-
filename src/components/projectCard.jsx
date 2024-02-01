06e422fc89d9d93867097bc2e19fbb54711eff9a
tidzi17 <tidzi15110@gmail.com>
2024-02-01 15:13:29 +0100

    pp

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -9,18 +9,18 @@ import { Link } from "react-router-dom";
 export default function ProjectCard({ id, title, date, image, imageID, description, techFirst, techSecond, codeurl, liveurl    }){
     const isScrollableImage = imageID !== "weather-app" && imageID !== "old-portfolio";
     return(
-        <div className="w-full h-[230px] border-[1px] border-black rounded-3xl p-4 flex flex-row gap-3 justify-between shadow-sm shadow-primaryGrey">
+        <div className="w-full h-[400px] md:h-[230px] border-[1px] border-black rounded-3xl p-4 flex flex-col md:flex-row gap-3 justify-between shadow-sm shadow-primaryGrey">
                {isScrollableImage ? (
-               <div className="w-1/2 h-full bg-black rounded-3xl overflow-hidden cursor-pointer">
+               <div className="w-full md:w-1/2 h-full bg-black rounded-3xl overflow-hidden cursor-pointer">
                    <Link to={liveurl} target="blank"><img src={image} alt="" className="grayscale w-full h-auto transform translate-x-0 transition duration-[10s] ease-in-out hover:-translate-y-[1300px] " /></Link>
                 </div>
                 
             ) : (
-                 <div className="w-1/2 h-full bg-black rounded-3xl relative">
+                 <div className="w-full md:w-1/2 h-full bg-black rounded-3xl relative">
                   <Link to={liveurl} target="blank"><img src={image} alt="" className="absolute inset-0 w-full h-full object-cover  rounded-3xl grayscale" /></Link>
                 </div>
             )}
-            <div className="w-1/2 flex flex-col items-center text-center">
+            <div className="w-full md:w-1/2 flex flex-col items-center text-center">
                 <h4 className="font-bold">{title}</h4>
                 <p className="text-xs md:text-sm mt-1">{description}</p>
                 <p className="text-base mt-1">Stack: <span>{techFirst}</span> <span>{techSecond}</span></p>
