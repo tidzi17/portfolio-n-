6b37de1cd5ca2b157d444522b6f4f78cc4c0bafb
tidzi17 <tidzi15110@gmail.com>
2024-07-03 15:02:03 +0200

    resume updatedd

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -8,7 +8,7 @@ import scssicon from '../assets/icons/sass.png';
 import { HashLink as Link } from "react-router-hash-link";
 import MotionDiv from '../components/animation';
 import { SlideY } from '../components/animation'; 
-import resume from '../assets/cv/TijanaDjudjicCV1.1.pdf';
+import resume from '../assets/cv/TijanaDjudjicResumeES1.pdf';
 
 export default function About(){
 
